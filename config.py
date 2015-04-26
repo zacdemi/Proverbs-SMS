7fcb1fd0aa142490e08fb4cd92b592b846f5abf5
1331susq <zac@1331susq>
2015-04-25 21:16:07 -0400

    added text history

--- config.py
+++
@@ -1,4 +0,0 @@
-
-secret_key = 'hh62h81h28h4892h3ll'
-password = "Gink1500"
-username = "zac.demi@gmail.com"
--- proverbs.py
+++ proverbs.py
@@ -10,6 +10,7 @@ db = client.bible
 collection = db.bible
 users = db.users
 carrier_list = {'AT&T':'@mms.att.net','Verizon':'@vtext.com'}
+appname = "Proverbs-SMS"
 
 def adduser(phone,carrier,taglist,frequency):
     count = users.find({"Phone":phone}).count()
@@ -20,11 +21,11 @@ def adduser(phone,carrier,taglist,frequency):
     if count > 0: #if user exist, update the confrim code and replace tags 
         update = users.update(
                 {"Phone":phone},{"$set": {"Confirmation":confirmation,"Tags":taglist,
-                "Frequency":frequency}})
+                    "Frequency":frequency,"History":[]}})
         return update
     else: #add a new user
         newuser = {"Phone":phone,"Carrier":carrier,"Tags":taglist,
-                "Confirmation":confirmation,"Frequency":frequency}
+                "Confirmation":confirmation,"Frequency":frequency,"History":"1:7"}
         users.insert(newuser)
 
 def subscribe_user(phone,action): #set action to Yes or No
@@ -54,11 +55,61 @@ def findrandom(taglist):
     #use random number to find random document
     randomverse = collection.find(
         {"Book":"Proverbs","Tags":{"$in":taglist}})[random.randrange(count)]
-    line = randomverse["Line"] 
-    chapter = str(randomverse["Chapter"])
-    verse = str(randomverse["Verse"])
-    return  line + " " + chapter  + " " + verse
- 
+    t = str(randomverse["Chapter"]) + ":" + str(randomverse["Verse"])
+    return  t
+
+
+#uses find random until a verse is found that doesn't exist
+def selectverse(phone):
+    user = users.find_one({"Phone":phone})   
+    taglist = user["Tags"]
+    history = user["History"]
+    max_amount = collection.find(
+        {"Book":"Proverbs","Tags":{"$in":taglist}}).count()
+
+    count = 0
+    while True:
+        verse_id = findrandom(taglist)
+        #verse already sent
+        if verse_id not in history:
+            push_new(phone,verse_id)
+            break
+        #all verses have been sent
+        if count >= max_amount:
+            verse_id = pop_new(phone)
+            break
+
+        count = count + 1
+        print count
+    return verse_id
+
+#adds new verse to end of history array
+def push_new(phone,verse_id):
+    users.update({"Phone":phone},{"$push":{"History":verse_id}})
+
+#user has used all tags
+#uses oldest and moves it to beginning of array
+def pop_new(phone):
+    #finds oldest proverb from history
+    oldest = users.find_one({"Phone":phone},{"History":{"$slice":1}})
+    oldest = str(oldest["History"]).strip("'[u]'")
+    #remove oldest provberb from history
+    users.update({"Phone":phone},{"$pop":{"History":-1}}) 
+    #append oldest proverb to end of history array
+    users.update({"Phone":phone},{"$push":{"History":oldest}})
+    return oldest 
+
+def make_message(verse_id):
+    chapter = int(verse_id.split(":")[0])
+    verse = int(verse_id.split(":")[1])
+
+    proverb = collection.find_one({"Book":"Proverbs","Chapter":chapter,"Verse":verse})
+    line = proverb["Line"]
+    message =  line + " " + str(chapter) + " " + str(verse)
+    #gmail won't send colons
+    message = message.replace(":",";")
+    return message
+
 def sendconfirm(phone):
     #establish a connection with gmail
     server = smtplib.SMTP( "smtp.gmail.com",587)
@@ -73,7 +124,7 @@ def sendconfirm(phone):
         message = ("The fear of the LORD is the beginning of knowledge; " 
                    "fools despise wisdom and instruction. 1 7")
     else:
-        message = str(user["Confirmation"])
+        message = "This is your " + appname + " confirmation code " + str(user["Confirmation"])
     #send text
     server.sendmail('Wisdom',address,message)
 
@@ -84,18 +135,20 @@ def sendproverbs():
     server.login(config.username,config.password)
 
     #loop through user database
-    subscribers = users.find() 
-    carriers = carriers.find()
+    subscribers = users.find({"Test":"Yes"}) 
     for each in subscribers:
         phone = each["Phone"]
         carrier = each["Carrier"]
         tags = each["Tags"]
         address = str(phone) + carrier_list[carrier]
-        message = findrandom(tags).replace(":",",")
-        #send text
-        server.sendmail('Wisdom',address,message)
+        message = make_message(selectverse(phone))
 
-#convert distinct tags into tuples
+        #send text
+        print "Sending out Proverbs to all subscribers"
+        server.sendmail('Wisdom',address,message)
+        print "Success"
+
+#convert distinct tags into tuples for checkboxes
 def distincttag():
     taglist = collection.distinct("Tags")
     tagtuple = [(x, x.capitalize()) for x in taglist]
